34b867305e3bb4f98ea368e639bfd0cfbbb2e853
daniela <danielaocampoo@gmail.com>
2020-10-28 11:28:42 -0500

    cambio prueba deply automatico

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
+  const express = require('express');
 
-const app = express();
+  const app = express();
 
-app.use(express.static('./dist/frontend'));
+  app.use(express.static('./dist/frontend'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: 'dist/frontend' });
-});
+  app.get('/*', function(req, res) {
+      res.sendFile('index.html', { root: 'dist/frontend' });
+  });
 
-app.listen(process.env.PORT || 8080);
+  app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 8080}`)
+  console.log(`Running on port ${process.env.PORT || 8080}`)
