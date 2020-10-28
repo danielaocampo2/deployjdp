9ae89ef9ff0b0d6fbdf465ad40e60f40abe14a29
daniela <danielaocampoo@gmail.com>
2020-10-28 10:40:33 -0500

    cambio en server

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-
-app.use(express.static(__dirname + '/dist/deployjdp'));
+app.use(express.static('./dist/deployjdp'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/deployjdp/index.html'));
+    res.sendFile('index.html', { root: 'dist/deployjdp/' });
 });
 
+
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
